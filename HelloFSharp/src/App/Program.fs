9174ffc3900589a4f5b0e681600656b81afaf8ca
“IvanARodriguez” <ivancalvo91@gmail.com>
2022-07-19 18:42:32 -0500

    Solution ready for execution with console output

--- HelloFSharp/src/App/Program.fs
+++ HelloFSharp/src/App/Program.fs
@@ -1,9 +1,18 @@
-﻿open System
-open Library
+﻿open Library
+open System
+
+module basicFunctions =
+
+    let squarePlus3 side = side * side + 3
+
+    let box = squarePlus3 500
+
+    printfn $"The result of squaring 5 and adding 3 is: %d{box}"
+
 
 [<EntryPoint>]
 let main args = 
-    printfn "Nice Command-Line arguments! Here's what the system.Text.Jsoin has to say about them"
+    printfn "Nice Command-Line arguments! Here's what the system.Text.Json has to say about them"
 
     let value, json = getJson {| args=args; year = DateTime.Now.Year |}
 
